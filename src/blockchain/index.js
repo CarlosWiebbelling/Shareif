c0c17c322b1d26d3cc53baf5eee3a3281b059f9e
CarlosWiebbelling <carloswiebbelling@outlook.com>
2019-10-29 19:03:08 -0300

    Crypto implemented

--- src/blockchain/index.js
+++ src/blockchain/index.js
@@ -1,36 +1,40 @@
+const crypto = require('crypto')
 const EC = require('elliptic').ec
-const ec = new EC('secp256k1')
-const { Blockchain, Block } = require('./blockchain')
 
+const ec = new EC('secp256k1')
+const length = 128
+
+const { Blockchain, Block } = require('./blockchain')
 const shareif = new Blockchain();
 
 // Users
-const user1 = ec.genKeyPair()
+const user2 = crypto.createDiffieHellman(length)
+user2.generateKeys('hex')
 
-console.log(user1)
+console.log(`Public key: ${ user2.getPublicKey() }\n`)
+console.log(`Private key: ${ user2.getPrivateKey() }`)
 
-// Chat 1
-const newBlock = new Block(
-  Date.parse('2017-01-01'), 
-  shareif.broadcast.getPublic('hex'),
-  user1.getPublic('hex'),
-  "This is the second message of the Shareif chat",
-  shareif.getLatestBlock().hash
-);
+// // Chat 1
+// const newBlock = new Block(
+//   Date.parse('2017-01-01'), 
+//   shareif.broadcast.getPublic,
+//   user2.getPublic,
+//   "This is the second message of the Shareif chat",
+//   shareif.getLatestBlock().hash
+// );
 
-newBlock.signTransaction(shareif.broadcast);
-shareif.addMessage(newBlock)
+// newBlock.signTransaction(shareif.broadcast);
+// shareif.addMessage(newBlock)
 
-const newBlock2 = new Block(
-  Date.parse('2017-01-01'), 
-  shareif.broadcast.getPublic('hex'),
-  user1.getPublic('hex'),
-  "This is the third message of the Shareif chat",
-  shareif.getLatestBlock().hash
-);
+// const newBlock2 = new Block(
+//   Date.parse('2017-01-01'), 
+//   shareif.broadcast.getPublic,
+//   user2.getPublic,
+//   "This is the third message of the Shareif chat",
+//   shareif.getLatestBlock().hash
+// );
 
-newBlock2.signTransaction(shareif.broadcast);
-shareif.addMessage(newBlock2)
+// newBlock2.signTransaction(shareif.broadcast);
+// shareif.addMessage(newBlock2)
 
-
-console.log(shareif)
+// console.log(shareif)
