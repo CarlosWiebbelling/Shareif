0082ecae524511447a3985d43cb68303e9e12993
Max <max.carlesso@hotmail.com>
2019-12-16 23:27:27 -0300

    Serialize encrypted messages

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-const crypto = require('crypto');
-const ecies = require('ecies-lite');
 const server = require('uWebSockets.js');
 
 const { node } = require('./src/node/index');
@@ -15,30 +13,36 @@ const page = fs.readFileSync('./src/public/index.html', 'utf-8');
 
 node.onMessage = (socket, message) => {
   console.log(`Received: ${message}`)
-  node.broadcast(message)
+  node.broadcast(message);
 }
 
 const handleCreatePair = (ws, isBinary, payload) => {
   const keys = Block.generateKeyPair();
   ws.send(JSON.stringify({ type: 'PONG_CREATE_PAIR', payload: keys }), isBinary);
+  console.log(shareif.chain);
   // console.log(node.getConnectionsAddress())
 };
 
 const handleSendMessage = (payload) => {
-  shareif.addMessage(new Block(
+  const block = new Block(
     Date.now(),
     payload.publicKey,
     payload.userId,
     payload.msg,
     shareif.getLatestBlock().hash
-  ));
+  );
 
-  console.log(shareif.getLatestBlock());
+  block.encrypt(payload.publicKey);
+  shareif.addMessage(block);
+
+  // console.log(shareif.getLatestBlock());
 };
 
 const handleSignIn = (payload) => {
-  console.log(`publicKey:${payload.publicKey}`);
-  console.log(`privateKey:${payload.privateKey}`);
+  const msg = shareif.getLatestBlock().getMessage(payload.privateKey);
+  console.log(msg);
+  // console.log(`publicKey:${payload.publicKey}`);
+  // console.log(`privateKey:${payload.privateKey}`);
 };
 
 server
@@ -46,7 +50,6 @@ server
   .ws('/*', {
     message: (ws, message, isBinary) => {
       const msg = JSON.parse(Buffer.from(message).toString());
-      console.log(msg)
       switch (msg.type) {
         case 'PING_CREATE_PAIR':
           handleCreatePair(ws, isBinary, msg.payload);
--- src/blockchain/block.js
+++ src/blockchain/block.js
@@ -1,6 +1,4 @@
 const crypto = require('crypto');
-const algorithm = 'aes-256-ctr';
-
 const ecies = require('ecies-lite');
 const ecdh = crypto.createECDH('secp256k1');
 
@@ -14,44 +12,83 @@ class Block {
   }
 
   encrypt(publicKey) {
-    this.message = ecies.encrypt(publicKey, Buffer.from(this.message));
-    this.calculateHash();
+    try {
+      const data = ecies.encrypt(
+        Buffer.from(publicKey, 'hex'),
+        Buffer.from(this.message)
+      );
+
+      this.message = {
+        epk: data.epk.toString('hex'),
+        iv: data.iv.toString('hex'),
+        ct: data.ct.toString('hex'),
+        mac: data.mac.toString('hex')
+      };
+
+      this.calculateHash();
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   decrypt(privateKey) {
-    return ecies.decrypt(ecdh.getPrivateKey(), this.message);
+    try {
+      const data = {
+        epk: Buffer.from(this.message.epk, 'hex'),
+        iv: Buffer.from(this.message.iv, 'hex'),
+        ct: Buffer.from(this.message.ct, 'hex'),
+        mac: Buffer.from(this.message.mac, 'hex')
+      };
+
+      return ecies.decrypt(
+        Buffer.from(privateKey, 'hex'),
+        data
+      );
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   calculateHash() {
-    this.hash = crypto
-      .createHash('sha256')
-      .update(this.previousHash + this.timestamp + this.message)
-      .digest('hex');
-  }
-
-  signTransaction(privateKey) {
-    if (this.toAddress === this.fromAddress)
-      throw new Error('You cannot send messages to yourself!');
-
-    this.encrypt(this.message, privateKey);
-    this.calculateHash();
+    try {
+      this.hash = crypto
+        .createHash('sha256')
+        .update(this.previousHash + this.timestamp + this.message)
+        .digest('hex');
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   isValid(previousBlock) {
-    if (this.previousHash !== previousBlock.hash) return false;
-    return true;
+    try {
+      if (this.previousHash !== previousBlock.hash) return false;
+      return true;
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   getMessage(privateKey) {
-    return this.decrypt(this.message, privateKey);
+    try {
+      const msg = this.decrypt(privateKey);
+      if (msg)
+        return msg.toString();
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   static generateKeyPair() {
-    let ecdh = crypto.createECDH('secp256k1');
-    ecdh.generateKeys();
-    return {
-      publicKey: ecdh.getPublicKey('hex'),
-      privateKey: ecdh.getPrivateKey('hex')
+    try {
+      ecdh.generateKeys();
+
+      return {
+        publicKey: ecdh.getPublicKey('hex'),
+        privateKey: ecdh.getPrivateKey('hex')
+      }
+    } catch (err) {
+      console.error(err);
     }
   }
 }
